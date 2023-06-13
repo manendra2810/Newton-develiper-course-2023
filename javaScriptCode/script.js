8428ff038c185040f6c779cb8ce469a3697607d4
Manendra Agnihotri <agnihotri2810@gmail.com>
2023-06-13 17:35:09 +0530

    use of grid property

---
+++ javaScriptCode/script.js
@@ -0,0 +1,24 @@
+'use strict'
+let arr=[1,2,3,4];
+
+arr.push(5);
+arr.pop();
+
+arr.unshift(5);
+arr.shift();
+
+let arr1=['A', 'b', 'c'];
+let newString = arr1.join('');
+
+console.log("new updated array :", newString);
+
+let arr2='1234';
+let newString1 = arr2.split('');
+
+console.log("new updated array :", newString1);
+
+var numbers=[1,2,3,4];
+const multiplyNums=numbers.mapping((number)=>{
+return (number*5);
+});
+console.log(multiplyNums);
